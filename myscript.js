71fb1aa3744fa8e06ea48229bc0821b5265e9f74
Asunder <trashemail3301@gmail.com>
2024-11-23 11:52:50 -0800

    human choice
    gives prompt to user. prompt ask to pick rock, paper, or scissors

--- myscript.js
+++ myscript.js
@@ -1,21 +1,29 @@
-console.log(getComputerChoice());
-
+// picks rock, paper or scissors randomly
 function getComputerChoice() {
-    let number = Math.floor(Math.random() * 2);
+    let number = Math.floor(Math.random() * 3); 
 
     switch(number) {
-        case 0 : number = "Rock";
-                break;
+        case 0 : number = "Rock";                    
+                    break;
         case 1 : number = "Paper";
-                break
-        case 2 : number = "Scissor"; 
-                break;
+                    break;
+        case 2 : number = "Scissors";
+                    break;
+        default : break;
     }
 
     return number;
-};
+}
 
 
+// gets human choice
+function getHumanChoice() {
+    let choice = prompt("Rock, paper or scissors?");
+    return choice;
+}
+
+let humanScore = 0;
+let comupterScore = 0;
 
 
 
