83dd7815e21cb1a949cb2b2aaeb8603b21df01ca
Asunder <trashemail3301@gmail.com>
2024-11-25 09:26:15 -0800

    game logic
    function plays 5 rounds and logs the round, choices, score and game winner.

--- myscript.js
+++ myscript.js
@@ -26,15 +26,63 @@ let humanScore = 0;
 let comupterScore = 0;
 
 
+// plays a singl round and determines winner of the round
+function playRound(humanChoice, computerChoice) {
+
+    humanChoice = humanChoice.toUpperCase();
+    computerChoice = computerChoice.toUpperCase();
+    console.log(humanChoice, computerChoice);
+
+    if (humanChoice == computerChoice ) {
+        console.log("Draw!");
+    } else if (humanChoice == "ROCK" && computerChoice == "PAPER") {
+        console.log("Computer Wins!");
+        comupterScore += 1;
+        humanScore += 0;
+    } else if (humanChoice == "ROCK" && computerChoice == "SCISSORS") {
+        console.log("You Win!");
+        comupterScore += 0;
+        humanScore += 1;
+    } else if (humanChoice == "PAPER" && computerChoice == "ROCK") {
+        console.log("You Win!");
+        comupterScore += 0;
+        humanScore += 1;
+    } else if (humanChoice == "PAPER" && computerChoice == "SCISSORS") {
+        console.log("Computer Wins!");
+        comupterScore += 1;
+        humanScore += 0;
+    } else if (humanChoice == "SCISSORS" && "ROCK") {
+        console.log("Computer Wins!");
+        comupterScore += 1;
+        humanScore +=  0;
+    } else if (humanChoice == "SCISSORS" && "PAPER") {
+        console.log("You Win!");
+        comupterScore += 0;
+        humanScore += 1;
+    }
+
+}
+   
+function playGame() {
+
+    console.log("Rock, Paper or Scissors?");
+
+    for (let i = 1; i <= 5; i++) {
+        console.log(`Round ${i}`);
+        playRound(getHumanChoice(), getComputerChoice());
+        console.log(`Player Score:${humanScore} Computer Score:${comupterScore}`);
+    }
+
+    if (humanScore > comupterScore)  {
+            console.log("You Win The Game!");
+    } else if (humanScore < comupterScore) {
+            console.log("computer Wins The Game!");
+    }    
+    
+}
 
 
-
-
-
-
-
-
-
+playGame();
 
 
 
